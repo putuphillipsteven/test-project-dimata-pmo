9319bb07ca9230a5ba7b03845bbad88ca4582813
phillip-steven <putu.phillip@gmail.com>
2024-04-04 23:29:17 +0700

    feat(default-route)
    add default route for dashboard

--- app/dashboard/(home)/page.tsx
+++ app/dashboard/(home)/page.tsx
@@ -1,7 +1,25 @@
+'use client';
+
 import { getDateAsString } from '@/app/lib/utils';
 import WidgetGrid from '@/app/ui/dashboard/(home)/widget-grid/widget-grid';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
-export default async function Page() {
+export default function Page() {
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
+	const { replace } = useRouter();
+	const params = new URLSearchParams(searchParams);
+	console.log(`pathname`, pathname);
+	console.log(`params`, params);
+
+	useEffect(() => {
+		params.set('tasks', 'upcoming');
+		params.set('project', 'recent');
+		params.set('people', 'frequentCollaborators');
+		replace(`${pathname}?${params.toString()}`);
+	}, []);
+
 	const currentDate: string = getDateAsString();
 	return (
 		<main className='h-full flex flex-col justify-center items-center p-8 gap-y-8'>
