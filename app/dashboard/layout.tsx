67e91884285dcb8863e0397437983b5391be35bd
phillip-steven <putu.phillip@gmail.com>
2024-04-04 22:50:33 +0700

    fix(layout)
    fixing  sidenav width

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 		<div className='flex flex-col w-screen min-h-screen'>
 			<Navbar />
 			<div className='flex' style={{ minHeight: `calc(100vh - 70px)` }}>
-				<div className='w-full flex-none md:w-64'>
+				<div className='w-full flex-none md:w-[16em]'>
 					<SideNav />
 				</div>
 				<div className='w-full md:overflow-y-auto bg-light-gray-dimata'>{children}</div>
