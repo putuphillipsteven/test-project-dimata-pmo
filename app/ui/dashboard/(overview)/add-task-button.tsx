89e5a70a2af09876e957b725afc129aa2664b662
phillip-steven <putu.phillip@gmail.com>
2024-04-10 10:51:25 +0700

    fix(test create page)

--- app/ui/dashboard/(overview)/add-task-button.tsx
+++ app/ui/dashboard/(overview)/add-task-button.tsx
@@ -1,10 +1,11 @@
+import Link from 'next/link';
 import { FaPlus } from 'react-icons/fa6';
 
 export default function AddTaskButton() {
 	return (
 		<button className='flex items-center gap-x-2 px-2 py-1 border border-black-dimata rounded-xl'>
 			<FaPlus />
-			<p>Add Task</p>
+			<Link href={'/dashboard/project/list/create'}>Add Task</Link>
 		</button>
 	);
 }
