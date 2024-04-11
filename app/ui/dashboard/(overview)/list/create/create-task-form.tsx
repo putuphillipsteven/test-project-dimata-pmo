954ba29aa2b491a8ddac22a6e5a6523bf6de9b56
phillip-steven <putu.phillip@gmail.com>
2024-04-11 16:54:49 +0700

    fix(create-form)
    add create-form.tsx

--- app/ui/dashboard/(overview)/list/create/create-task-form.tsx
+++ app/ui/dashboard/(overview)/list/create/create-task-form.tsx
@@ -3,3 +3,5 @@
 import { TaskField } from '@/app/lib/definition';
 import { useFormState } from 'react-dom';
 import Link from 'next/link';
+
+export default function CreateTaskForm({ tasks }: { tasks: TaskField }) {}
