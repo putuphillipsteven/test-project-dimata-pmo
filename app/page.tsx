5f8c1c96445319f01473474fd858e208af716663
phillip-steven <putu.phillip@gmail.com>
2024-04-12 12:11:41 +0700

    fix(link home)
    give blue text

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-center p-24'>
 			<h1 className='font-bold uppercase'>Test Project Dimata IT Solutions</h1>
-			<Link href={'/dashboard'}>Dashboard</Link>
+			<Link href={'/dashboard'} className='text-blue-500 underline'>
+				Dashboard
+			</Link>
 		</main>
 	);
 }
